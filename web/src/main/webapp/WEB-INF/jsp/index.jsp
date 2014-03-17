0f7dc5da43c206db0e55d1254ac3eb1f31466734
douglyuckling <douglas.w.paul@gmail.com>
2014-03-16 23:13:31 -0400

    Tasks can be added, but aren't synced to server

--- web/src/main/webapp/WEB-INF/jsp/index.jsp
+++ web/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -8,8 +8,22 @@
 </head>
 <body>
 <div class="container">
-    <h3>Tasks</h3>
-    <ul id="tasks_list" class="list-group"></ul>
+    <div class="row" style="margin-bottom: 1em;">
+        <div class="col-lg-12">
+            <h3>Tasks</h3>
+            <div class="input-group">
+                <input id="add_task_summary_text" type="text" class="form-control" placeholder="Add a task...">
+                <span class="input-group-btn">
+                    <button id="add_task_btn" type="button" class="btn btn-primary">Add Task</button>
+                </span>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <ul id="tasks_list" class="list-group"></ul>
+        </div>
+    </div>
 </div>
 </body>
 </html>
--- web/src/main/webapp/js/app/main.js
+++ web/src/main/webapp/js/app/main.js
@@ -1,24 +1,55 @@
 define(['d3',
+        'jquery',
         'backbone'],
 function(d3,
+         $,
          Backbone) {
     "use strict";
 
     var tasks = new Backbone.Collection;
     tasks.url = '/data/tasks/';
-
     tasks.fetch({
         success: function() {
-            var tasksSelection = d3.select('#tasks_list').selectAll('ul').data(tasks.toArray(), function(d) {return d.id;});
-
-            tasksSelection.enter()
-                .append('ul')
-                .attr('class', 'list-group-item');
-            tasksSelection.exit()
-                .remove();
-            tasksSelection
-                .text(function(d) { return d.get('summary'); });
+            renderTasks();
         }
     });
+
+    $('#add_task_btn').click(function() {
+        addTask();
+    });
+    $('#add_task_summary_text').keypress(function(event) {
+        if (event.which == 13) {
+            addTask();
+        }
+    });
+
+    function addTask() {
+        var taskData = collectNewTaskFormData();
+        tasks.add([taskData]);
+        renderTasks();
+        clearNewTaskForm();
+    }
+
+    function collectNewTaskFormData() {
+        return {
+            summary: $('#add_task_summary_text').val()
+        }
+    }
+
+    function clearNewTaskForm() {
+        $('#add_task_summary_text').val('');
+    }
+
+    function renderTasks() {
+        var tasksSelection = d3.select('#tasks_list').selectAll('ul').data(tasks.toArray(), function(d) {return d.id || d.cid;});
+
+        tasksSelection.enter()
+            .append('ul')
+            .attr('class', 'list-group-item');
+        tasksSelection.exit()
+            .remove();
+        tasksSelection
+            .text(function(d) { return d.get('summary'); });
+    }
 
 });
